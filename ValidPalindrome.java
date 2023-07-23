5909cbd540df6a39722af6aede515e55077fb3a1
Santhosh.S <69361129+Santhosh553@users.noreply.github.com>
2023-07-23 08:53:49 +0530

    leetcode 125

--- ValidPalindrome.java
+++ ValidPalindrome.java
@@ -3,7 +3,6 @@ public class ValidPalindrome{
         public static void main(String[] args){
                 Scanner sc=new Scanner(System.in);
                 String s=sc.next();
-
                 StringBuilder sb=new StringBuilder();
                
                 String ostr=s.replaceAll("[^A-Za-z0-9]","").toLowerCase();
