55858f48fe3eb524885b60491603cf878e344155
Santhosh S <santhosh.senthil552003@gmail.com>
2024-08-14 06:39:14 +0530

    leetcode 20

---
+++ Easy/ValidParentheses.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+import java.util.Scanner;
+public class ValidParentheses {
+    
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
+        }
+        return stack.isEmpty();
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(isValid(s));
+    }
+}
